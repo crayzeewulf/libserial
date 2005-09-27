f19125a1b67297fddd078b4555698846b54e266e
CrayzeeWulf <crayzeewulf@users.noreply.github.com>
2005-09-27 04:34:18 +0000

    - Removed redundant include guards. - Minor reformating. - Changed the serial port device to /dev/ttyS0 instead of /dev/tts/usb/0.

--- libserial/examples/read_port.cpp
+++ libserial/examples/read_port.cpp
@@ -1,11 +1,5 @@
-#ifndef _std_iostream_INCLUDED_
-#    include <iostream>
-#    define _std_iostream_INCLUDED_
-#endif
-
-#ifndef _SerialStream_h_
-#    include <SerialStream.h>
-#endif
+#include <iostream>
+#include <SerialStream.h>
 
 int
 main( int    argc,
@@ -16,53 +10,57 @@ main( int    argc,
     //
     using namespace LibSerial ;
     SerialStream serial_port ;
-    serial_port.Open( "/dev/usb/tts/0" ) ;
-    if ( ! serial_port.good() ) {
-        std::cerr << "Error: Could not open serial port." << std::endl ;
+    serial_port.Open( "/dev/ttyS0" ) ;
+    if ( ! serial_port.good() ) 
+    {
+        std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
+                  << "Error: Could not open serial port." 
+                  << std::endl ;
         exit(1) ;
     }
     //
     // Set the baud rate of the serial port.
     //
     serial_port.SetBaudRate( SerialStreamBuf::BAUD_57600 ) ;
-    if ( ! serial_port.good() ) {
+    if ( ! serial_port.good() ) 
+    {
         std::cerr << "Error: Could not set the baud rate." << std::endl ;
         exit(1) ;
     }
-
     //
     // Set the number of data bits.
     //
     serial_port.SetCharSize( SerialStreamBuf::CHAR_SIZE_8 ) ;
-    if ( ! serial_port.good() ) {
+    if ( ! serial_port.good() ) 
+    {
         std::cerr << "Error: Could not set the character size." << std::endl ;
         exit(1) ;
     }
-
     //
     // Disable parity.
     //
     serial_port.SetParity( SerialStreamBuf::PARITY_NONE ) ;
-    if ( ! serial_port.good() ) {
+    if ( ! serial_port.good() ) 
+    {
         std::cerr << "Error: Could not disable the parity." << std::endl ;
         exit(1) ;
     }
-
     //
     // Set the number of stop bits.
     //
     serial_port.SetNumOfStopBits( 1 ) ;
-    if ( ! serial_port.good() ) {
+    if ( ! serial_port.good() ) 
+    {
         std::cerr << "Error: Could not set the number of stop bits."
                   << std::endl ;
         exit(1) ;
     }
-
     //
     // Turn on hardware flow control.
     //
     serial_port.SetFlowControl( SerialStreamBuf::FLOW_CONTROL_HARD ) ;
-    if ( ! serial_port.good() ) {
+    if ( ! serial_port.good() ) 
+    {
         std::cerr << "Error: Could not use hardware flow control."
                   << std::endl ;
         exit(1) ;
@@ -75,11 +73,12 @@ main( int    argc,
     //
     // Keep reading data from serial port and print it to the screen.
     //
-    while( serial_port.rdbuf()->in_avail() > 0  ) {
-      char next_byte;
-      serial_port.get(next_byte);
-      std::cerr << std::hex << (int)next_byte << " ";
-    };	    
+    while( serial_port.rdbuf()->in_avail() > 0  ) 
+    {
+        char next_byte;
+        serial_port.get(next_byte);
+        std::cerr << std::hex << (int)next_byte << " ";
+    } 
     std::cerr << std::endl ;
     return EXIT_SUCCESS ;
 }
