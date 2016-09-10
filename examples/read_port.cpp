e14f97efc090a33ccdcff3aa115a8e5a26ffaa04
mcsauder <mcsauder@greypointcorp.com>
2016-09-10 00:32:51 -0600

    Revert read_example and write_example variable underscore naming convention to original and revert spacing convention to match the library.

--- examples/read_port.cpp
+++ examples/read_port.cpp
@@ -5,87 +5,84 @@
 
 using namespace LibSerial;
     
-int main(int argc, char** argv )
+int main()
 {
-    (void) argc; // Quiet compiler warning.
-    (void) argv; // Quiet compiler warning.
-
     // Instantiate the SerialStream object then open the serial port.
-    SerialStream serialStream;
-    serialStream.Open("/dev/ttyUSB0");
+    SerialStream serial_stream;
+    serial_stream.Open("/dev/ttyUSB0");
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
         std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] "
                   << "Error: Could not open serial port." 
-                  << std::endl;
-        exit(1);
+                  << std::endl ;
+        exit(1) ;
     }
 
     // Set the baud rate of the serial port.
-    serialStream.SetBaudRate(SerialStreamBuf::BAUD_115200);
+    serial_stream.SetBaudRate(SerialStreamBuf::BAUD_115200);
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
-        std::cerr << "Error: Could not set the baud rate." << std::endl;
-        exit(1);
+        std::cerr << "Error: Could not set the baud rate." << std::endl ;
+        exit(1) ;
     }
 
     // Set the number of data bits.
-    serialStream.SetCharSize(SerialStreamBuf::CHAR_SIZE_8);
+    serial_stream.SetCharSize( SerialStreamBuf::CHAR_SIZE_8 ) ;
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
-        std::cerr << "Error: Could not set the character size." << std::endl;
-        exit(1);
+        std::cerr << "Error: Could not set the character size." << std::endl ;
+        exit(1) ;
     }
 
     // Disable parity.
-    serialStream.SetParity(SerialStreamBuf::PARITY_NONE);
+    serial_stream.SetParity( SerialStreamBuf::PARITY_NONE ) ;
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
-        std::cerr << "Error: Could not disable the parity." << std::endl;
-        exit(1);
+        std::cerr << "Error: Could not disable the parity." << std::endl ;
+        exit(1) ;
     }
 
     // Set the number of stop bits.
-    serialStream.SetNumOfStopBits(1);
+    serial_stream.SetNumOfStopBits(1);
     
-    if (!serialStream.good()) 
+    if (!serial_stream.good()) 
     {
         std::cerr << "Error: Could not set the number of stop bits."
-                  << std::endl;
-        exit(1);
+                  << std::endl ;
+        exit(1) ;
     }
 
     // Turn off hardware flow control.
-    serialStream.SetFlowControl(SerialStreamBuf::FLOW_CONTROL_NONE);
-    if (!serialStream.good()) 
+    serial_stream.SetFlowControl(SerialStreamBuf::FLOW_CONTROL_NONE);
+    if ( !serial_stream.good() ) 
     {
         std::cerr << "Error: Could not use hardware flow control."
-                  << std::endl;
-        exit(1);
+                  << std::endl ;
+        exit(1) ;
     }
 
     // Do not skip whitespace characters while reading from the serial port.
     // serialStream.unsetf(std::ios_base::skipws);
 
     // Wait for some data to be available at the serial port.
-    while(serialStream.rdbuf()->in_avail() == 0) 
+    while( serial_stream.rdbuf()->in_avail() == 0 ) 
     {
-        usleep(100);
+        usleep(100) ;
     }
 
     // Keep reading data from serial port and print it to the screen.
-    while(serialStream.rdbuf()->in_avail() > 0) 
+    while( serial_stream.rdbuf()->in_avail() > 0 ) 
     {
         char nextByte;
-        serialStream.get(nextByte);
-        std::cerr << std::hex << static_cast<int>(nextByte) << " ";
-        usleep(100);
+        serial_stream.get(nextByte);
+        std::cerr << std::hex << static_cast<int>( nextByte ) << " " ;
+        usleep(100) ;
     }
 
-    std::cerr << std::endl;
-    return EXIT_SUCCESS;
+    std::cerr << std::endl ;
+    return EXIT_SUCCESS ;
 }
--- examples/write_port.cpp
+++ examples/write_port.cpp
@@ -9,103 +9,100 @@ using namespace LibSerial;
 // file to the serial port one character at a time.
 int main(int argc, char** argv)
 {
-    (void) argc; // Quiet compiler warning.
-    (void) argv; // Quiet compiler warning.
-
-    if (argc < 2) 
+    if ( argc < 2 ) 
     {
-        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
-        return 1;
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl ;
+        return 1 ;
     }
 
     // Open the serial port.
-    const char* const SERIAL_PORT_DEVICE = "/dev/ttyUSB0";
+    const char* const SERIAL_PORT_DEVICE = "/dev/ttyUSB0" ;
     
-    SerialStream serialStream;
-    serialStream.Open(SERIAL_PORT_DEVICE);
+    SerialStream serial_stream;
+    serial_stream.Open( SERIAL_PORT_DEVICE ) ;
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
         std::cerr << "Error: Could not open serial port " 
                   << SERIAL_PORT_DEVICE 
-                  << std::endl;
-        exit(1);
+                  << std::endl ;
+        exit(1) ;
     }
 
     // Set the baud rate of the serial port.
-    serialStream.SetBaudRate(SerialStreamBuf::BAUD_115200);
+    serial_stream.SetBaudRate( SerialStreamBuf::BAUD_115200 ) ;
     
-    if (!serialStream.good()) 
+    if (!serial_stream.good()) 
     {
-        std::cerr << "Error: Could not set the baud rate." << std::endl;
-        exit(1);
+        std::cerr << "Error: Could not set the baud rate." << std::endl ;
+        exit(1) ;
     }
 
     // Set the number of data bits.
-    serialStream.SetCharSize(SerialStreamBuf::CHAR_SIZE_8);
+    serial_stream.SetCharSize( SerialStreamBuf::CHAR_SIZE_8 ) ;
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
-        std::cerr << "Error: Could not set the character size." << std::endl;
-        exit(1);
+        std::cerr << "Error: Could not set the character size." << std::endl ;
+        exit(1) ;
     }
 
     // Disable parity.
-    serialStream.SetParity(SerialStreamBuf::PARITY_NONE);
+    serial_stream.SetParity( SerialStreamBuf::PARITY_NONE ) ;
     
-    if (!serialStream.good()) 
+    if (!serial_stream.good()) 
     {
-        std::cerr << "Error: Could not disable the parity." << std::endl;
-        exit(1);
+        std::cerr << "Error: Could not disable the parity." << std::endl ;
+        exit(1) ;
     }
 
     // Set the number of stop bits.
-    serialStream.SetNumOfStopBits(1);
+    serial_stream.SetNumOfStopBits(1) ;
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
         std::cerr << "Error: Could not set the number of stop bits."
-                  << std::endl;
-        exit(1);
+                  << std::endl ;
+        exit(1) ;
     }
 
     // Turn on hardware flow control.
-    serialStream.SetFlowControl(SerialStreamBuf::FLOW_CONTROL_NONE);
+    serial_stream.SetFlowControl( SerialStreamBuf::FLOW_CONTROL_NONE ) ;
     
-    if (!serialStream.good()) 
+    if ( !serial_stream.good() ) 
     {
         std::cerr << "Error: Could not use hardware flow control."
-                  << std::endl;
-        exit(1);
+                  << std::endl ;
+        exit(1) ;
     }
 
     // Do not skip whitespace characters while reading from the serial port.
-    // serialStream.unsetf(std::ios_base::skipws);
+    // serial_stream.unsetf(std::ios_base::skipws);
 
     // Open the input file for reading. 
-    std::ifstream input_file(argv[1]);
+    std::ifstream input_file( argv[1] );
     
-    if (!input_file.good()) 
+    if ( !input_file.good() ) 
     {
         std::cerr << "Error: Could not open file "
-                  << argv[1] << " for reading." << std::endl;
-        return 1;
+                  << argv[1] << " for reading." << std::endl ;
+        return 1 ;
     }
 
     // Read characters from the input file and dump them to the serial port. 
-    std::cerr << "Dumping file to serial port." << std::endl;
+    std::cerr << "Dumping file to serial port." << std::endl ;
     
-    while(input_file) 
+    while( input_file ) 
     {
         char nextByte;
-        input_file.read( &nextByte, 1 );
-        serialStream.write( &nextByte, 1 );
+        input_file.read( &nextByte, 1 ) ;
+        serial_stream.write( &nextByte, 1 ) ;
 
         // Print a '.' for every character read from the input file. 
-        std::cerr << ".";
+        std::cerr << "." ;
     }
 
-    std::cerr << std::endl;
-    std::cerr << "Done." << std::endl;
-    return EXIT_SUCCESS;
+    std::cerr << std::endl ;
+    std::cerr << "Done." << std::endl ;
+    return EXIT_SUCCESS ;
 }
