e51f6e663e380f5508513787c6afe87df264db8d
CrayzeeWulf <crayzeewulf@gmail.com>
2018-10-04 21:11:35 -0700

    Updated unit tests (Boost) to make sure that failure to open a SerialStream thrown OpenFailed and also sets failbit.

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,21 +1,44 @@
-#include <SerialStreamBuf.h>
 #include <SerialStream.h>
+#include <SerialStreamBuf.h>
 
 #define BOOST_TEST_MODULE libserial
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( SerialStreamBuf_Constructor_Test )
+BOOST_AUTO_TEST_CASE( SerialStreamBuf_Constructor_Test ) // NOLINT (cert-err58-cpp)
 {
-    using namespace LibSerial;
-    SerialStreamBuf buf;
-    BOOST_CHECK( false == buf.IsOpen() );
+    // 
+    // SerialStreamBuf should not be open on default construction
+    //
+    using LibSerial::SerialStreamBuf ;
+    SerialStreamBuf buf ;
+    BOOST_CHECK( false == buf.IsOpen() ); // NOLINT (cppcoreguidelines-pro-type-vararg)
 }
 
-BOOST_AUTO_TEST_CASE( SerialStream_Constructor_Test )
+BOOST_AUTO_TEST_CASE( SerialStream_Constructor_Test ) // NOLINT (cert-err58-cpp)
 {
-    using namespace LibSerial;
-    SerialStream serial_stream;
-    BOOST_CHECK( false == serial_stream.IsOpen() );
+    using LibSerial::SerialStream ;
+    //
+    // SerialStream should not be open on default construction
+    //
+    {
+        SerialStream serial_stream;
+        BOOST_CHECK( false == serial_stream.IsOpen() ); // NOLINT (cppcoreguidelines-pro-type-vararg)
+    }
+    //
+    // Attempting to open a non-existent serial port should leave the 
+    // serial stream in non-good state.
+    //
+    {
+        SerialStream serial_stream ;
+        // NOLINTNEXTLINE (cppcoreguidelines-pro-type-vararg)
+        BOOST_CHECK_THROW(
+            serial_stream.Open("/dev/some_non_existent_device_hope_it_does_not_exist"),
+            LibSerial::OpenFailed
+        ) ;
+        // NOLINTNEXTLINE (cppcoreguidelines-pro-type-vararg)
+        BOOST_CHECK(not serial_stream.good()) ;
+    }
 }
+
