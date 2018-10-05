a8d865c8aaef9d282e64f452dcd07f3a9f9ba1ff
Crayzee Wulf <crayzeewulf@gmail.com>
2018-10-04 20:35:25 -0700

    Fixed indentation and various updates based on clang-tidy.
    See http://clang.llvm.org/extra/clang-tidy/

--- examples/main_page_example.cpp
+++ examples/main_page_example.cpp
@@ -5,49 +5,53 @@
 #include <SerialPort.h>
 #include <SerialStream.h>
 
-using namespace LibSerial;
+#include <iostream>
 
 int main()
 {
-   // Instantiate a Serial Port and a Serial Stream object.
-   SerialPort serial_port;
-   SerialStream serial_stream;
+    using LibSerial::SerialPort ;
+    using LibSerial::SerialStream ;
+    // Instantiate a Serial Port and a Serial Stream object.
+    SerialPort serial_port;
+    SerialStream serial_stream;
 
-   // Open the hardware serial ports.
-   serial_port.Open( "/dev/ttyUSB0" );
-   serial_stream.Open( "/dev/ttyUSB1" );
+    // Open the hardware serial ports.
+    serial_port.Open( "/dev/ttyUSB0" );
+    serial_stream.Open( "/dev/ttyUSB1" );
 
-   // Set the baud rates.
-   serial_port.SetBaudRate( BaudRate::BAUD_115200 );
-   serial_stream.SetBaudRate( BaudRate::BAUD_115200 );
+    // Set the baud rates.
+    using LibSerial::BaudRate;
+    serial_port.SetBaudRate( BaudRate::BAUD_115200 );
+    serial_stream.SetBaudRate( BaudRate::BAUD_115200 );
 
-   char write_byte_1 = 'a';
-   char write_byte_2 = 'b';
+    char write_byte_1 = 'a';
+    char write_byte_2 = 'b';
 
-   char read_byte_1 = 'A';
-   char read_byte_2 = 'B';
+    char read_byte_1 = 'A';
+    char read_byte_2 = 'B';
 
-   // Write a character.
-   serial_port.WriteByte(write_byte_1);
-   serial_stream << write_byte_2;
+    // Write a character.
+    serial_port.WriteByte(write_byte_1);
+    serial_stream << write_byte_2;
 
-   size_t timeout_milliseconds = 5;
+    size_t timeout_milliseconds = 5;
 
-   try
-   {
-      // Read a character.
-      serial_port.ReadByte(read_byte_1, timeout_milliseconds);
-      serial_stream >> read_byte_2;
-   }
-   catch (ReadTimeout)
-   {
-     std::cerr << "The Read() call has timed out." << std::endl;
-   }
+    using LibSerial::ReadTimeout;
+    try
+    {
+        // Read a character.
+        serial_port.ReadByte(read_byte_1, timeout_milliseconds);
+        serial_stream >> read_byte_2;
+    }
+    catch (const ReadTimeout&)
+    {
+        std::cerr << "The Read() call has timed out." << std::endl;
+    }
 
-   std::cout << "serial_port read:   " << read_byte_1 << std::endl;
-   std::cout << "serial_stream read: " << read_byte_2 << std::endl;
+    std::cout << "serial_port read:   " << read_byte_1 << std::endl;
+    std::cout << "serial_stream read: " << read_byte_2 << std::endl;
 
-   // Close the Serial Port and Serial Stream.
-   serial_port.Close();
-   serial_stream.Close();
+    // Close the Serial Port and Serial Stream.
+    serial_port.Close();
+    serial_stream.Close();
 }
